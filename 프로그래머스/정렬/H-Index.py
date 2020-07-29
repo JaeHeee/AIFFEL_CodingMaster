35c18e7a82e2a989ab1f118212df65ba96939558
JaeHeee <kjha2142@gmail.com>
2020-07-29 17:22:55 +0900

    H-Index

--- 프로그래머스/정렬/H-Index.py
+++ 프로그래머스/정렬/H-Index.py
@@ -1,12 +1,13 @@
 def solution(citations):
     answer = 0
+    # H-index가 될수 있는 최댔값은 n
     n = len(citations)
     citations.sort()
-
-    # H-Index 찾기
-    for idx, c in enumerate(citations):
-        if n-idx >= c:
-            answer = c
-
+    
+    for i, c in enumerate(citations):
+        # i = 나머지 논문의 수, n-i = c번 이상 인용된 논문의 수
+        if n-i <= c:
+            return n-i
+        
+        
     return answer
-
