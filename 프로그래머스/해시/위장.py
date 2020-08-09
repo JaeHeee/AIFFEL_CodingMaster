67c960cdfe7775cc5ac2d3f18c3847af122ce113
JaeHeee <kjha2142@gmail.com>
2020-08-09 20:23:27 +0900

    위장.py

--- 프로그래머스/해시/위장.py
+++ 프로그래머스/해시/위장.py
@@ -1,13 +1,15 @@
 def solution(clothes):
     answer = 1
     clothes_d = {}
-
+    
+    # key = 의상의 종류, value = 의상의 이름
     for c in clothes:
         if c[1] in clothes_d.keys():
             clothes_d[c[1]].append(c[0])
         else:
             clothes_d[c[1]] = [c[0]]
 
+    # 아무것도 입지 않는 상태를 포함하여 가능한 경우의 수를 구합니다.
     for v in clothes_d.values():
         answer *= (len(v)+1)
 
