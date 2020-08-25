c5fe8c1f166ec1fdf131c3f7f63185578c4b59cd
JaeHeee <kjha2142@gmail.com>
2020-08-25 14:08:08 +0900

    이중우선순위큐

--- 프로그래머스/힙/이중우선순위큐.py
+++ 프로그래머스/힙/이중우선순위큐.py
@@ -6,15 +6,17 @@ def solution(operations):
 
     for op in operations:
         op, num = op.split(" ")
-
+        # heap 구조를 만듭니다.
         if op == 'I':
             heapq.heappush(heap,int(num))
         elif op =='D' and heap:
+            # D 다음의 숫자가 양수이면 max_heap구조로 변환하고, max 삭제
             if int(num)>0:
                 heapq._heapify_max(heap)
                 heapq.heappop(heap)
                 if heap:
                     heapq.heapify(heap)
+            # 음수이면 min 삭제
             else:
                 heapq.heappop(heap)
     
